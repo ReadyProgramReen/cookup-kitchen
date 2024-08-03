b4fcc76a1b11be2f0be4f35f35e16a6f8f10b05a
Maureen E <maureenekeugo1@gmail.com>
2024-08-03 19:54:20 -0400

    Added and styled header componet

--- src/Components/Head/Head.jsx
+++ src/Components/Head/Head.jsx
@@ -3,7 +3,7 @@ import '../Head/Head.css'
 
 const Head = () => {
   return (
-    <div className='Header'>
+    <div className='header'>
         <div className="header-content">
             <h2>Order your favorite food here</h2>
             <p>Choose from a diverse food menu featuring a delectable array of carribean dishes crafted with the finest ingredient and culinary expertise, one delicious meal at a time</p>
