5c13886e0d8cde95cf61aac9fd35c3caf9c8c0a9
Maureen E <maureenekeugo1@gmail.com>
2024-08-03 17:10:04 -0400

    Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+
+
 
 const App = () => {
   return (
     <div className='app'>
+
       <Navbar/>
+      <Routes>a
+        <Route path='/' element={<Home/>}/>
+        <Route path ='/order' element={<PlaceOrder/>}/>
+        
+
+
+
+      </Routes>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+    </div>
   )
 }
 
