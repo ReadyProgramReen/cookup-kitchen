b0cdc19fa2453defe36709f3d30223453f286aac
Maureen E <maureenekeugo1@gmail.com>
2024-08-04 18:18:51 -0400

    Created add to cart funtionailty and remove from cart  funtionality

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -1,9 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null)
 
 const StoreContextProvider =(props)=>{
+
+    const [cartItem, setCartItem] = useState({});
+
+    // add to card
+
+    const addToCart=(itemid)=>{
+        if(!cartItem[itemid]){
+            setCartItem((prev)=>({...prev,[itemid]:1}))
+        }
+        else{
+            setCartItem((prev)=>({...prev,[itemid]:prev[itemid]+1}))
+        }
+
+    }
+
+    // remove from card
+
+    const removeFromCart =(itemid)=>{
+        setCartItem((prev)=>({...prev, [itemid]:prev[itemid]-1}))
+
+    }
 
     const contextValue ={
         food_list
