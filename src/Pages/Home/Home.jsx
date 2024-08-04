0e1da55f791176def5844831bd1b5729e62de177
Maureen E <maureenekeugo1@gmail.com>
2024-08-04 18:00:25 -0400

    Added and styled both fooddisplay and fooditem component and added funtionality

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from '../../Components/Head/Head'
 import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+  const [category,setCategory] = useState('All')
   return (
     <div>
       <Head/>
-      <ExploreMenu/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisplay category={category}/> 
     </div>
   )
 }
