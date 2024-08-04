3ee4ae1900b92486bfe2991a1bfe80958b3171c7
Maureen E <maureenekeugo1@gmail.com>
2024-08-03 21:03:31 -0400

    Added and styled explore menu component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Head from '../../Components/Head/Head'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 const Home = () => {
   return (
     <div>
       <Head/>
+      <ExploreMenu/>
     </div>
   )
 }
