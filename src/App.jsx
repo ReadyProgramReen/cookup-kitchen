7df3ec3a76f4cd8e319b233548e6a8e871be94b4
Maureen E <maureenekeugo1@gmail.com>
2024-08-05 14:16:43 -0400

    Added and styled footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
 import CartItem from './Pages/CartItem/CartItem'
+import Footer from './Components/Footer/Footer'
 
 
 
 const App = () => {
   return (
+    <>
     <div className='app'>
+    
 
       <Navbar/>
       <Routes>a
         <Route path='/' element={<Home/>}/>
         <Route path ='/order' element={<PlaceOrder/>}/>
         <Route path='/cart' element={<CartItem/>}/>
-        
-        
-
-
-
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
