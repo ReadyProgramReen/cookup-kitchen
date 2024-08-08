6edf1149cbdb5aa367a8296b16d61a854840bf07
Maureen E <maureenekeugo1@gmail.com>
2024-08-08 15:20:28 -0400

    accessed the cart item and foodlist from our context into cart page

--- src/Pages/Carts/Carts.jsx
+++ src/Pages/Carts/Carts.jsx
@@ -18,6 +18,23 @@ const Carts = () => {
             </div>
             <br />
             <hr />
+
+            {food_list.map((item,index)=>{
+                if(cartItem[item._id] > 0){
+                    return (
+                        <div className="cart-item-title cart-item-item">
+                            <img src={item.image} alt="" />
+                            <p>{item.name}</p>
+                            <p>{item.price}</p>
+                            <p>{cartItem[item._id]}</p>
+                            <p>{item.price *cartItem[item._id]}</p>
+                            <p>x</p>
+
+                        </div>
+                    )
+                }
+
+            })}
         </div>
     </div>
   )
