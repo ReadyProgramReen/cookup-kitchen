37f3f0b3cb31e2024d05745584f6a1dfa2a127cf
Maureen E <maureenekeugo1@gmail.com>
2024-08-10 18:36:57 -0400

    added functionality to connect admin add page to mongodb db

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,13 @@ import { Route, Routes } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import Order from './pages/Order/Order'
 import List from './pages/List/List'
+import { ToastContainer } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className='app'>
+      <ToastContainer/>
 
       <Navbar/>
       
