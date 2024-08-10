1cd8f6f062f11898055c3739081cb8c1ad12b8a8
Maureen E <maureenekeugo1@gmail.com>
2024-08-10 16:12:52 -0400

    Set up route for order,list and add pages

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add/Add'
+import Order from './pages/Order/Order'
+import List from './pages/List/List'
 
 const App = () => {
   return (
@@ -11,6 +15,13 @@ const App = () => {
       <hr/>
       <div className="">
       <Sidebar/>
+      <Routes>
+        <Route path='/add' element={<Add/>}/>
+        <Route path='/list' element={<List/>}/>
+        <Route path='/order' element={<Order/>}/>
+
+
+      </Routes>
       </div>
     </div>
   )
---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Add/Add.css'
+
+const Add = () => {
+  return (
+    <div>Add</div>
+  )
+}
+
+export default Add
