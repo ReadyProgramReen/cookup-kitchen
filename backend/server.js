2825ab68d0a8baad2083c4397e719606ac93f286
Maureen E <maureenekeugo1@gmail.com>
2024-08-13 10:23:18 -0400

    created order api using stripe payment method.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 
@@ -29,6 +30,8 @@ app.use('/api/food', foodRouter);
 app.use('/images',express.static('uploads'))
 app.use('/api/user', userRouter)
 app.use('/api/cart', cartRouter)
+app.use('/api/order', orderRouter)
+
 
 app.get('/', (req,res)=>{
     res.send('API Working')
